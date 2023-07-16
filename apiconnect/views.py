4ae4cc2efe24c841193c93a271eaa63101ea1581
rajabi-j <117579205+rajabi-j@users.noreply.github.com>
2023-07-16 17:14:18 -0400

    New modules: search form, processing search form, creating url request, parsing the XML response, Showing the response in table format

--- apiconnect/views.py
+++ apiconnect/views.py
@@ -2,30 +2,103 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 import xml.etree.ElementTree as ET
+import urllib.parse
+from .parseXML import XmlDictConfig
 
 
 def home(request):
-    url = "https://classic.clinicaltrials.gov/api/query/study_fields?expr=heart+attack&fields=NCTId%2CBriefTitle%2CCondition&min_rnk=1&max_rnk=&fmt=xml"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if the request was unsuccessful
+    return render(request, 'apiconnect/home.html')
 
+
+def results(request):
+
+    if request.method == 'POST':
+        search_query = ''
+        result_rnk = 1
+        try:
+            search_query = request.POST['search_query']
+            result_rnk = int(request.POST['result_rnk'])
+        except:
+            search_query = createSearch(request.POST)
+        url_query = createURL(search_query, result_rnk)    
+        response_xml = getData(url_query)
+        context = toDict(response_xml)
+        context.update({'search_query': search_query})
+        min_rnk = int(context['MinRank'])
+        max_rnk = int(context['MaxRank'])
+        num_studies = int(context['NStudiesFound'])
+        if min_rnk > 1:           
+            context.update({'pre_rank': min_rnk - 10 if min_rnk > 10 else 1})
+            print('pre_rank: ', context['pre_rank'])
+        if max_rnk < num_studies:                        
+            context.update({'next_rank': min_rnk + 10})
+            print('next_rank: ', context['next_rank'])
+        
+        return render(request, 'apiconnect/results.html', context)
+
+
+    for field in context['StudyFieldsList']['StudyFields']:
+        print(field)
+
+    return render(request, 'apiconnect/results.html', context)
+
+
+def createSearch(reqForm):
+    
+    searchQ = "" # Reset searchQ
+
+    condition = reqForm['conditionSearch']
+    country = reqForm['countryFilter']
+    state = reqForm['stateFilter']
+    city = reqForm['cityFilter']
+    range = reqForm['rangeFromCity']
+    ageRange = reqForm['ageRange']
+    ageValues = ageRange.split("-");
+    
+    searchQ += condition;
+    if country or state or city:
+        searchQ += " AND SEARCH[Location]("
+        searchQ += "AREA[LocationCountry]" + country if country else ""
+        searchQ += " AND " if (country and (state or city)) else ""
+        searchQ += "AREA[LocationState]" + state if state else ""
+        searchQ += " AND " if (state and city) else ""
+        searchQ += "AREA[LocationCity]" + city if (state and city) else ""
+        searchQ += ")"
+
+    # age search
+    searchQ += " AND (AREA[MinimumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years]";
+    searchQ += " AND AREA[MaximumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years])";
+
+    return searchQ
+
+    
+def createURL(searchQ, result_rnk):
+    query = "https://clinicaltrials.gov/api/query/study_fields?expr=" 
+    query += urllib.parse.quote_plus(searchQ)
+    query += "&fields=NCTId%2CBriefTitle%2CCondition%2CLocationCity%2CLocationState%2CLocationCountry%2CMinimumAge%2CMaximumAge"
+    query += "&min_rnk=" + str(result_rnk) + "&max_rnk=" + str(result_rnk+9) + "&fmt=xml"
+    
+    return query
+    
+
+def getData(url):
+    try:
+        print(url)
+        xml_response = requests.get(url)
+        xml_response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        return xml_response.content
         # Print the response content
         # print(response.text)
 
     except requests.exceptions.RequestException as e:
     # An error occurred
         print("Error: ", e)
-    root = ET.fromstring(response.content)
-    content = ''
 
-    for child in root.find('StudyFieldsList'):
-        print(child.tag, child.attrib)
-        for ch2 in child.find('FieldValues'):
-            print(ch2.text)
-        # content += child.tag + '  '
-    return HttpResponse(content)
-    #return render(request, 'apiconnect/home.html')
+def toDict(xml_content):
+    root = ET.XML(xml_content)
+    xmldict = XmlDictConfig(root)
 
-def results(request):
-    return render(request, 'apiconnect/results.html')
+    return xmldict
+
+
+
