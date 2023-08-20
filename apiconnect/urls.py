2c09733058109ba35bc22dda42e8f2fe691aac0f
rajabi-j <117579205+rajabi-j@users.noreply.github.com>
2023-08-20 09:10:43 -0400

    User authentication added. Card view and single view results added. pagination added.

--- apiconnect/urls.py
+++ apiconnect/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('results/', views.results, name='results'),
+    path('cardresults/', views.cardResults, name='cardResults'),
+    path('singleresult/<str:ntcId>/', views.singleResult, name='singleResult'),
 ]
--- apiconnect/views.py
+++ apiconnect/views.py
@@ -1,14 +1,111 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import requests
 from django.http import HttpResponse
 import xml.etree.ElementTree as ET
 import urllib.parse
 from .parseXML import XmlDictConfig
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
     return render(request, 'apiconnect/home.html')
 
+def loginPage(request):
+
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')
+
+        user = authenticate(request, username=username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist!')
+
+    context = {'page': page}
+    return render(request, 'apiconnect/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registeration')
+
+    return render(request, 'apiconnect/login_register.html', {'form': form})
+
+
+def cardResults(request):
+
+    if request.method == 'POST':
+        url_query = ''
+        result = ''
+        next_token = ''
+        pre_token = False
+        page_tokens = ''
+        result_rnk = 1
+        if 'search_query' in request.POST:
+            url_query = request.POST['search_query']
+            result_rnk = int(request.POST['result_rnk']) if 'result_rnk' in request.POST else 1
+            next_token = request.POST['next_token'] if 'next_token' in request.POST else ''
+            pre_token = True if 'pre_token' in request.POST else False
+            page_tokens = request.POST['page_tokens'] if 'page_tokens' in request.POST else ''
+        else:
+            url_query = createSearch(request.POST)
+
+        if next_token :
+            result = getData(url_query + '&pageToken=' + next_token)
+            page_tokens += (',' if page_tokens else '') + next_token
+        elif pre_token and (',' in page_tokens):
+            page_tokens = page_tokens.rsplit(',', 1)[0] if page_tokens else ''
+            result = getData(url_query + '&pageToken=' + page_tokens.split(',')[-1])
+        else:
+            page_tokens = ''
+            result = getData(url_query)
+
+        print(page_tokens)
+        result.update({'result_rnk': result_rnk})
+        result.update({'search_query': url_query})          
+        result.update({'page_tokens': page_tokens})
+        if page_tokens :   result.update({'pre_page': 1})
+        return render(request, 'apiconnect/cardResults.html', result)
+
+
+    return HttpResponse("Error!")
+
+
+def singleResult(request, ntcId):
+    url_query = "https://clinicaltrials.gov/api/v2/studies/" + ntcId 
+    fields = "NCTId,BriefTitle,OfficialTitle,Condition,BriefSummary,DetailedDescription,LocationCountry,LocationState,LocationCity"
+    url_query += "?fields=" + urllib.parse.quote_plus(fields)
+    response_json = getData(url_query)
+    return render(request, 'apiconnect/singleResult.html', response_json)
 
 def results(request):
 
@@ -55,38 +152,45 @@ def createSearch(reqForm):
     ageRange = reqForm['ageRange']
     ageValues = ageRange.split("-");
     
-    searchQ += condition;
+    searchQ += "&query.cond=" + urllib.parse.quote_plus(condition)
+
+    locationQ = ''
     if country or state or city:
-        searchQ += " AND SEARCH[Location]("
-        searchQ += "AREA[LocationCountry]" + country if country else ""
-        searchQ += " AND " if (country and (state or city)) else ""
-        searchQ += "AREA[LocationState]" + state if state else ""
-        searchQ += " AND " if (state and city) else ""
-        searchQ += "AREA[LocationCity]" + city if (state and city) else ""
-        searchQ += ")"
+        locationQ += "AREA[LocationCountry]" + country if country else ""
+        locationQ += " AND " if (country and (state or city)) else ""
+        locationQ += "AREA[LocationState]" + state if state else ""
+        locationQ += " AND " if (state and city) else ""
+        locationQ += "AREA[LocationCity]" + city if (state and city) else ""
+        searchQ += "&query.locn=" + urllib.parse.quote_plus(locationQ)
 
     # age search
-    searchQ += " AND (AREA[MinimumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years]";
-    searchQ += " AND AREA[MaximumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years])";
+    ageQ = ''
+    ageQ += "AREA[MinimumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years]"
+    ageQ += " AND AREA[MaximumAge] RANGE[" + ageValues[0] + " years, " + ageValues[1] + " years]"
+    searchQ += '&filter.advanced=' + urllib.parse.quote_plus(ageQ)
 
-    return searchQ
+    query = 'https://clinicaltrials.gov/api/v2/studies?format=json'
+    query += searchQ
+    query += '&fields=NCTId%2CBriefTitle%2CCondition&countTotal=true'
+    
+    return query
 
     
 def createURL(searchQ, result_rnk):
-    query = "https://clinicaltrials.gov/api/query/study_fields?expr=" 
+    query = "https://clinicaltrials.gov/api/v2/studies?format=json&query.cond=" 
     query += urllib.parse.quote_plus(searchQ)
-    query += "&fields=NCTId%2CBriefTitle%2CCondition%2CLocationCity%2CLocationState%2CLocationCountry%2CMinimumAge%2CMaximumAge"
-    query += "&min_rnk=" + str(result_rnk) + "&max_rnk=" + str(result_rnk+9) + "&fmt=xml"
+    query += "&fields=NCTId%2CBriefTitle%2CCondition%2CLocationCity%2CLocationState%2CLocationCountry"
+    query += "&min_rnk=" + str(result_rnk) + "&max_rnk=" + str(result_rnk+9) + "&fmt=json"
     
     return query
     
 
 def getData(url):
     try:
-        print(url)
-        xml_response = requests.get(url)
-        xml_response.raise_for_status()  # Raise an exception if the request was unsuccessful
-        return xml_response.content
+        json_response = requests.get(url)
+        json_response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        json_context = json_response.json()
+        return json_context
         # Print the response content
         # print(response.text)
 
